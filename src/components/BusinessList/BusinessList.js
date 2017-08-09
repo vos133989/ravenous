026a4db4a58969ce65b320c032f542785502de0b
Ruud Naastepad <ruudnaastepad@hotmail.com>
2017-08-09 13:09:08 +0200

    Ravenous part 2

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './BusinessList.css';
 import {Business} from '../Business/Business';
 
-class BusinessList extends React.Component {
+export class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map(business => {
+        return  <Business business={business}/>;
+        })}
       </div>
     );
   }
 }
-
-export {BusinessList};
